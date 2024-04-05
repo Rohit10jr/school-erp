b34459df5754ee2e829d992272c7fb593e6f9c4a
Rohit10jr <rohitjworkspace@gmail.com>
2024-04-05 15:44:26 +0530

    academy model serializers

---
+++ academics/forms.py
@@ -0,0 +1,132 @@
+from email.policy import default 
+from logging import PlaceHolder
+from pyexpat import model
+from django import forms 
+from .models import *
+
+class Questionform(forms.ModelForm):
+    class Meta:
+        model = Question
+        fields = "__all__"
+
+    def __init__(self, *args, **kwargs):
+        super(Questionform, self).__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control'
+
+class Answerform(forms.ModelForm):
+    class Meta:
+        model = Answers
+        fields = '__all__'
+        exclude = ['question']
+
+class Chapterform(forms.ModelFrom):
+    class Meta:
+        model = Chapter
+        fields = '__all__'
+        exclude = ['subject']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            if field_name != 'allchapter':
+                print(type(field_name), field_name)
+                field.widget.attrs['class'] = 'form-control'
+
+class Subjectform(forms.ModelForm):
+    class Meta:
+        model = Subject
+        fields = '__all__'
+        exclude = ['grade']
+
+class Loginform(forms.Form):
+    email = forms.EmailField(widget=forms.TextInput(
+        attrs={"class": "form-control", "placeholder": "Email"}))
+    phone = forms.CharField(widget=forms.TextInput(
+        attrs={"class": "form-control", "placeholder": "Phone"}))
+
+class grade_form(forms.ModelForm):
+    class Meta:
+        model = Grade
+        fields = ['grade']
+
+class subject_form(forms.ModelForm):
+    class Meta:
+        model = Subject
+        fields = '__all__'
+
+class chapter_form(forms.ModelForm):
+    class Meta:
+        model = Chapter
+        fields = '__all__'
+
+class chapterlist_form(forms.ModelForm):
+    grade = forms.CharField(max_length=14)
+    subject = forms.CharField(max_length=20)
+
+class subjectlist_form(forms.Form):
+    grade = forms.CharField(max_length=14)
+
+class questionlist_form(forms.ModelForm):
+    no_of_questions = forms.CharField(max_length=20)
+
+    class Meta:
+        model = Question
+        fields = ['grade', 'subject', 'chapter', 'no_of_questions']
+
+class question_form(forms.ModelFrom):
+    class Meta:
+        model = Question
+        fields = '__all__'
+
+class form_for_chapterlist(forms.ModelForm):
+    class Meta:
+        model = Subject
+        fields = ['grade']
+
+class answer_form(forms.ModelForm):
+    ans = question_form
+
+    class Meta:
+        model = Answers
+        fields = '__all__'
+        exclude = ['question']
+
+class TestForm(forms.ModelForm):
+    timing = forms.IntegerField(min_value=0, widget=forms.NumberInput(attrs={
+        'placeholder':'duration in seconds'
+    }))
+    overall_marks = forms.IntegerField(min_vlaue=0)
+
+    class Meta:
+        model = Test
+        fields = ['timing', 'overall_marks', 'remarks', 'description', 'pass_percentage']
+
+class instruction_form(forms.Form):
+    instruction = forms.CharField(widget=forms.Textarea(
+        attrs={'class': 'form-control', 'placeholder':'instructions', 'style':'color:black; widh:50%'}
+    ))
+
+
+class questionCustomForm(forms.Form):
+    from_chapter = forms.ChoiceField()
+    to_Chapter = forms.ChoiceField()
+    all_chapter = forms.BooleanField()
+
+    def __init__(self, *args, **kwargs):
+        super(questionCustomForm, self).__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            if field_name != 'allChapter':
+                print(type(field_name), field_name)
+                field.widget.attrs['class'] = 'form-control'
+
+
+class customizeForm(forms.Form):
+    chapter = forms.ChoiceField()
+    congnitive_level = forms.ChoiceField()
+    no_cognitive_level = forms.IntegerField(label='')
+    difficulty_level = forms.ChoiceField()
+    no_difficulty_level = forms.IntegerField(label='')
+
+
+
--- academics/models.py
+++ academics/models.py
@@ -72,12 +72,134 @@ class Chapter(models.Model):
         ordering = ('subject', 'chapter_no')
 
 
+questiontype_choice = (
+    ('MCQ', 'mcq'),
+    ('Fill_In_The_Blanks', 'fill_in_the_blanks'),
+    ('Match_The_Following', 'match_the_following')
+)
+
+cognitive_level = (
+    ('Knowledge','knowledge'),
+    ('Comprehension','corprehension'),
+    ('Application','application')
+)
+
+difficulty_level = (
+    ('Easy', 'easy'),
+    ('Medium','medium'),
+    ('Hard', 'hard')
+)
+
+
+class Question(models.Model):
+    grade = models.ForeignKey(Grade, on_delete=models.CASCADE, null=True)
+    subject = models.ForeignKey(Subject, on_delete=models.CASCADE, null=True)
+    chapter = models.ForeignKey(Chapter, on_delete=models.CASCADE, null=True )
+    question = models.CharField(max_length=50)
+    duration = models.PositiveIntegerField()
+    mark = models.PositiveIntegerField(default=1)
+    chapter_no = models.IntegerField(defualt=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+    question_type = models.charField(
+        max_length = 20,
+        choices = questiontype_choice,
+        default = questiontype_choice[0][0])
+    
+    congnitive_level = models.CharField(
+        max_length=20, 
+        choices=cognitive_level, 
+        default=cognitive_level[0][0])
+    
+    difficulty_level = models.CharField(
+        max_length=20, 
+        choices=difficulty_level,
+        default=difficulty_level[0][0])
+    
+    def __str__(self):
+        return self.question
+
+
+answer_choices = (
+    ('option_a', 'option_a'),
+    ('option_b', 'option_b'),
+    ('option_C', 'option_c'),
+    ('option_D', 'option_d')
+)
+
+class Answers(models.Model):
+    question = models.OneToOneField(Question, on_delete=models.CASCADE, null=True)
+    option_a = models.CharField(max_length=50, null=True)
+    option_b = models.CharField(max_length=50, null=True)
+    option_c = models.CharField(max_length=50, null=True)
+    option_d = models.CharField(max_length=50, null=True)
+
+    answer = models.CharField(max_length=50, 
+                              choices=answer_choices, 
+                              default=answer_choices[0][0])
+    
+    def __str__(self):
+        return self.answer
+
+
+class Question_Paper(models.Model):
+    grade = models.ForeignKey(Grade, on_delete=models.CASCADE, null=True)
+    subject = models.ForeignKey(Subject, on_delete=models.CASCADE, null=True)
+    file = models.FileField(upload_to='question_files/', null=True, blank=True)
+    created_by = models.CharField(max_length=20)
+    created_at = models.DateTimeField(auto_now_add=True)
+    test_id = models.CharField(max_length=30, null=True, blanl=True)
+    no_of_questions = ArrayField(models.CharField(max_length=10, blanl=True, default=list))
+    timing = models.IntegerField(defualt=0)
+    overall_marks = models.IntegerField(Default=0)
+
+    def __str__(self):
+        return (str())
+    
+    def save(self, *args, **kwargs):
+        if self.test_id:
+            test = Test.objects.get(test_id=self.test_id)
+            test_marks = self.overall_marks
+            test.duration = self.timing
+            test.save()
+        super(Question_Paper, self).save(*args, **kwargs)
+    class Meta:
+        ordering = ('grade', 'subject', '-created_at')
+
+
+
+class Test(models.Model):
+    question_paper = models.ForeignKey(Question_Paper, on_delete=models.SET_NULL, null=True)
+    grade = models.ForeignKey(Grade, on_delete=models.SET_NULL, null=True)
+    subject = models.ForeignKey(Subject, on_delete=models.SET_NULL, null=True)
+    duration = models.PositiveIntegerField()
+    created_staff_id = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    marks = models.PositiveIntegerField()
+    remarks = models.CharField(max_length=50)
+    description = models.CharField(max_length=50)
+    test_id = models.CharField(max_length=30, null=True, blank=True)
+    pass_percentage = models.PositiveIntegerField(defauklt=35)
+
+    def __str__(self):
+        return self.remarks 
+    
+    def save(self, *args, **kwargs):
+        if not self.test_id:
+            self.test_id = (str(uuid.uuid4()))[:16]
+        question_paper = self.question_paper
+        if not self.duration:
+            self.duration = question_paper.timing
+        if not self.marks:
+            self.marks = question_paper.overall_marks
+        super(Test, self).save(*args, **kwargs)
 
 
 class TestResult(models.Model):
     student_id = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     grade = models.ForeignKey(Grade, on_delete=models.SET_NULL, null=True)
     subject = models.ForeignKey(Subject, on_delete=models.SET_NULL, null=True) 
+    test_id = models.ForeignKey(Test, on_delete=models.DO_NOTHING, null=True)
+    question_paper = models.ForeignKey(
+        Question_Paper, on_delete=models.DO_NOTHING, null=True)
 
     result = models.CharField(max_length=10)
     score = models.PositiveIntegerField()
