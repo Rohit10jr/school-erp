aa1b91f00bd3d7345c57e20f1e355869ef3d2b0c
Rohit10jr <rohitjworkspace@gmail.com>
2024-04-07 22:47:22 +0530

    utils

--- academics/FrontendUrl.py
+++ academics/FrontendUrl.py
@@ -28,6 +28,45 @@ def render_to_pdf(template_src, folder_name, params:dict):
     
     return filename, True
 
+def render_to_pdf2(template_src, folder_name, question_paper, params: dict):
+    # print(questionb)
+    template = get_template(template_src)
+    folder_name = folder_name
+    html = template.render(params)
+    result = BytesIO()
+    filename = str(uuid.uuid4())
+    pdf = pisa.pisaDocument(BytesIO(html.encode("UTF-8")), result)
+    exists = False
+
+    if not question_paper:
+        grade = params['grade']
+        subject = params['subject']
+        register_number = params['register_number']
+
+        for file in os.listdir((str(settings.BASE_DIR)+f'/media/{folder_name}/')):
+            filename = (str(file))
+            if register_number in filename:
+                exists = True
+        filename = f'grade-{grade}&&subject-{subject}{register_number}'
+        try:
+            if exists:
+                os.rename((str(settings.BASE_DIR)+f'/media/{folder_name}/{file}'), (str(
+                    settings.BASE_DIR)+f'/media/{folder_name}/{filename}.pdf'))
+            with open(str(settings.BASE_DIR)+f'/media/{folder_name}/{filename}.pdf', 'wb+') as output:
+                pdf = pisa.pisaDocument(BytesIO(html.encode("UTF-8")), output)
+        except Exception as e:
+            print(e)
+        return filename, True
+
+    if pdf.err:
+        print('error')
+        return '', False
+    questionfile = SimpleUploadedFile(
+        filename+'.pdf', result.getvalue(), content_type='application/pdf')
+    print(questionfile)
+    question_paper.file = questionfile
+    question_paper.save()
+    return question_paper, True
 
 
 
